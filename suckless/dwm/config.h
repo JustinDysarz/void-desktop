27210012462302f409ba23414e0777bf9831ce10
Justin Dysarz <justindysarz@proton.me>
2025-12-15 12:47:34 -0600

    Add libtool

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -2,11 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 35;       /* snap pixel */
-static const unsigned int gappih    = 35;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 35;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 35;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 35;       /* vert outer gap between windows and screen edge */
+static const unsigned int snap      = 0;       /* snap pixel */
+static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
